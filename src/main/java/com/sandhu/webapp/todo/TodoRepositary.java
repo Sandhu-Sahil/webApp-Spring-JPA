636521a46c1148d548fd738fd23619f20c9577ce
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2024-02-09 11:58:44 +0530

    WebApp JSP and JSPF

---
+++ src/main/java/com/sandhu/webapp/todo/TodoRepositary.java
@@ -0,0 +1,9 @@
+package com.sandhu.webapp.todo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepositary extends JpaRepository<Todo, Integer>{
+	public List<Todo> findByUsername(String username);
+}
